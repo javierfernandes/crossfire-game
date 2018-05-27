73597e7c7f5a482ba211909648cea30f397bcd48
Javier Fernandes <javier.fernandes@gmail.com>
2018-05-26 21:11:57 -0300

    Adding sprite tag

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactPhaser from 'react-phaser-renderer'
 
 ReactPhaser.render(
-  <game width="270" height="480" />, 
+  <game width="270" height="480">
+    <sprite x={100} y={100} name="batman" /> 
+  </game>, 
   document.querySelector('#content')
 )
