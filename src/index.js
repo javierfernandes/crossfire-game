519784003aaabbf44bd6b87b6e2f9b7047606b1a
Javier Fernandes <javier.fernandes@gmail.com>
2018-06-20 16:42:46 -0300

    adding scene

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import MovingImage from './components/MovingImage.jsx'
 
 ReactPhaser.render(
   <game width="800" height="600">
-    <sprite x={100} y={100} name="batman" />
-
-    <MovingImage />
+    <scene name="welcome" autoStart={true}>
+      <sprite x={100} y={100} name="batman" />
+      <MovingImage />
+    </scene>
   </game>, 
   document.querySelector('#content')
 )
